0c8d7763c3bde20dfb2d14cbee3ce6bb13dfb01a
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-15 15:19:50 +0200

    Add the context provider

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'english' ? 'Your Name' : 'Dein Name';
+
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
