d940b364a92097c10920dba54e76a5498add4664
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-15 14:17:37 +0200

    Implement functionality for selecting a language

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag de"
+            onClick={() => this.onLanguageChange('german')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
